19a9d84d0e19149fee96af0a4d346c02f9244509
Sergey Adamchuk <adamchuk.mail@gmail.com>
2022-02-04 20:46:17 +0200

    Test terraform Webserver ver1

---
+++ WebServer.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+
+resource "aws_instance" "my_webserver" {
+  ami                    = "ami-0b0cd98f300749df1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+  tags = {
+    Name = "My WebServer"
+  }
+
+
+
+  user_data = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2> WebServer wit IP: $myip</h2><br>Build by Terraform!" > /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+
+EOF
+}
+
+resource "aws_security_group" "my_webserver" {
+  name        = "WebServer Security Group"
+  description = "My First Security Group"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
